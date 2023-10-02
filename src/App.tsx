6ea9b1a55fb8ddce62ab9164aab99d1398695e88
KerryFerguson04 <kcf@udel.edu>
2023-10-02 03:17:54 -0400

    Got rid of some warnings.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import vulpix from "./vulpix.png";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
